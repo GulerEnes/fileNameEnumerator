8d969e62ea499e4037d61323af033e39fd04034c
GulerEnes <kriptoenes@gmail.com>
2021-05-06 01:58:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from os.path import splitext
+from os import walk, rename
+
+path = input("Absolute Path: ")
+fileExt = list(input("File extentions (write comma seperated): ").split(","))
+start = int(input("Min number for naming: "))
+step = int(input("Step number for naming: "))
+try:
+    _, _, filenames = next(walk(path))
+    print(filenames)
+    for f in filenames:
+        oldName, ext = splitext(f)
+        if ext[1:] in fileExt:
+            rename(path + '/' + f, path + '/' + str(start) + ext)
+            start += step
+
+
+except Exception as e:
+    print("Error:", e)
